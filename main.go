eaf7bc354ed24ce30818118adfd54dabf94a39c6
Bogdan I. Bursuc <bogdanbursuc86@gmail.com>
2017-03-10 16:51:15 +0200

    Torrent downloading

--- main.go
+++ main.go
@@ -25,61 +25,64 @@ func main() {
 		},
 	}
 	app.Action = func(c *cli.Context) error {
-		if c.Bool("debug") {
-			log.SetLevel(log.DebugLevel)
-		}
-
-		if c.NArg() == 0 {
-			log.Error("Torrent file not specified.")
-			return fmt.Errorf("Torrent file is required.")
-		}
-
-		path := c.Args().First()
-		log.WithField("file", path).Debug("Fetching torrent file")
-
-		client, err := torrent.NewClient(&torrent.Config{Debug: true})
+		client, err := torrent.NewClient(&torrent.Config{
+			DataDir:    os.TempDir(),
+			Debug:      true,
+			Seed:       true,
+			NoUpload:   false,
+			DisableTCP: false,
+			ListenAddr: ":50007",
+		})
 		if err != nil {
 			log.WithError(err).Error("Error creating torrent client")
 			return err
 		}
 
-		t, err := client.AddTorrentFromFile(path)
+		t, err := client.AddMagnet("magnet:?xt=urn:btih:bc37ea8ae2d0dfec5bd9d6800b3e0af9399e4deb&dn=The.Big.Bang.Theory.S10E18.HDTV.x264-LOL&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969&tr=udp%3A%2F%2Fzer0day.ch%3A1337&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Fpublic.popcorn-tracker.org%3A6969")
 		if err != nil {
-			log.WithError(err).Error("Error adding torrent file")
+			log.WithError(err).Error("Error adding magnet torrent")
 			return err
 		}
 
-		// Wait for info
-		<-t.GotInfo()
-		t.DownloadAll()
-		mi := t.Info()
-		log.WithFields(log.Fields{
-			"name":   mi.Name,
-			"length": mi.Length,
-			"source": mi.Source,
-			"files":  len(mi.Files),
-		}).Info("Torrent metainfo")
+		go func() {
+			// Wait for info
+			<-t.GotInfo()
+			t.DownloadAll()
 
-		movieFile := getMovieFile(t)
-		log.WithField("moviefile", movieFile.Path()).Info("Movie File")
+			mi := t.Info()
+			log.WithFields(log.Fields{
+				"name":   mi.Name,
+				"length": mi.Length,
+				"source": mi.Source,
+				"files":  len(mi.Files),
+			}).Info("Torrent metainfo")
 
-		r := t.NewReader()
-		defer r.Close()
+			movieFile := getMovieFile(t)
+			log.WithField("moviefile", movieFile.Path()).Info("Movie File")
 
-		rs := missinggo.NewSectionReadSeeker(r, movieFile.Offset(), movieFile.Length())
-		p := make([]byte, 0, 1024)
-		n, err := rs.Read(p)
-		if err != nil {
-			log.WithError(err).Error("Error reading torrent file")
-			return err
-		}
-		log.WithField("lenght", n).Info("Read number of bytes")
+			r := t.NewReader()
+			defer r.Close()
+
+			rs := missinggo.NewSectionReadSeeker(r, movieFile.Offset(), movieFile.Length())
+			p := make([]byte, 1024)
+			n, err := rs.Read(p)
+			if err != nil {
+				log.WithError(err).Error("Error reading torrent file")
+				return
+			}
+			log.WithField("lenght", n).Info("Read number of bytes")
+		}()
 
 		for {
-
-			fmt.Println("Bytes completed: ", t.BytesCompleted())
-			time.Sleep(time.Second)
+			select {
+			case <-t.Closed():
+				fmt.Println("Torrent is closed.")
+			default:
+				fmt.Println("Torrent stats: ", t.Stats())
+				time.Sleep(time.Second)
+			}
 		}
+
 		return nil
 	}
 
