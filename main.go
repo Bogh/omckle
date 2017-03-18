ffa686e6584887e9e89e5dbe75aaacdf2f269dde
Bogdan I. Bursuc <bogdanbursuc86@gmail.com>
2017-03-18 15:39:58 +0200

    UI to upload torrents and download

--- main.go
+++ main.go
@@ -1,104 +1,111 @@
 package main
 
 import (
-	"fmt"
-	"os"
-	"time"
+	"net/http"
 
-	log "github.com/Sirupsen/logrus"
-	"github.com/anacrolix/missinggo"
-	"github.com/anacrolix/torrent"
-
-	"github.com/urfave/cli"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	app := cli.NewApp()
+	router := gin.Default()
+	router.Static("/static", "./static")
+	router.LoadHTMLGlob("templates/*")
 
-	app.Name = "Omckle"
-	app.Usage = "torrent streaming"
-	app.ArgsUsage = "<file>"
-	app.Flags = []cli.Flag{
-		cli.BoolFlag{
-			Name:  "debug, d",
-			Usage: "Enable debug logging",
-		},
-	}
-	app.Action = func(c *cli.Context) error {
-		client, err := torrent.NewClient(&torrent.Config{
-			DataDir:    os.TempDir(),
-			Debug:      true,
-			Seed:       true,
-			NoUpload:   false,
-			DisableTCP: false,
-			ListenAddr: ":50007",
+	router.
+		GET("/", func(c *gin.Context) {
+			c.HTML(http.StatusOK, "index.gohtml", nil)
+		}).
+		POST("/", func(c *gin.Context) {
+			ctx := gin.H{}
+			switch c.DefaultPostForm("format", "magnet") {
+			case "magnet":
+				magnetLink := c.PostForm("magnet")
+				if magnetLink == "" {
+					ctx["Error"] = "Magnet url is required."
+				}
+				err := AddMagnetLink(magnetLink)
+				if err == nil {
+					ctx["Success"] = true
+				} else {
+					ctx["Error"] = err.Error()
+				}
+			}
+			c.HTML(http.StatusOK, "index.gohtml", ctx)
 		})
-		if err != nil {
-			log.WithError(err).Error("Error creating torrent client")
-			return err
-		}
 
-		t, err := client.AddMagnet("magnet:?xt=urn:btih:bc37ea8ae2d0dfec5bd9d6800b3e0af9399e4deb&dn=The.Big.Bang.Theory.S10E18.HDTV.x264-LOL&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969&tr=udp%3A%2F%2Fzer0day.ch%3A1337&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Fpublic.popcorn-tracker.org%3A6969")
-		if err != nil {
-			log.WithError(err).Error("Error adding magnet torrent")
-			return err
-		}
+	router.Run(":8080")
+	// app := cli.NewApp()
 
-		go func() {
-			// Wait for info
-			<-t.GotInfo()
-			t.DownloadAll()
+	// app.Name = "Omckle"
+	// app.Usage = "torrent streaming"
+	// app.ArgsUsage = "<file>"
+	// app.Flags = []cli.Flag{
+	// 	cli.BoolFlag{
+	// 		Name:  "debug, d",
+	// 		Usage: "Enable debug logging",
+	// 	},
+	// }
+	// app.Action = func(c *cli.Context) error {
+	// 	client, err := torrent.NewClient(&torrent.Config{
+	// 		DataDir:    os.TempDir(),
+	// 		Debug:      true,
+	// 		Seed:       true,
+	// 		NoUpload:   false,
+	// 		DisableTCP: false,
+	// 		ListenAddr: ":50007",
+	// 	})
+	// 	if err != nil {
+	// 		log.WithError(err).Error("Error creating torrent client")
+	// 		return err
+	// 	}
 
-			mi := t.Info()
-			log.WithFields(log.Fields{
-				"name":   mi.Name,
-				"length": mi.Length,
-				"source": mi.Source,
-				"files":  len(mi.Files),
-			}).Info("Torrent metainfo")
+	// 	t, err := client.AddMagnet("magnet:?x.t=urn:btih:bc37ea8ae2d0dfec5bd9d6800b3e0af9399e4deb&dn=The.Big.Bang.Theory.S10E18.HDTV.x264-LOL&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969&tr=udp%3A%2F%2Fzer0day.ch%3A1337&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Fpublic.popcorn-tracker.org%3A6969")
+	// 	if err != nil {
+	// 		log.WithError(err).Error("Error adding magnet torrent")
+	// 		return err
+	// 	}
 
-			movieFile := getMovieFile(t)
-			log.WithField("moviefile", movieFile.Path()).Info("Movie File")
+	// 	go func() {
+	// 		// Wait for info
+	// 		<-t.GotInfo()
+	// 		t.DownloadAll()
 
-			r := t.NewReader()
-			defer r.Close()
+	// 		mi := t.Info()
+	// 		log.WithFields(log.Fields{
+	// 			"name":   mi.Name,
+	// 			"length": mi.Length,
+	// 			"source": mi.Source,
+	// 			"files":  len(mi.Files),
+	// 		}).Info("Torrent metainfo")
 
-			rs := missinggo.NewSectionReadSeeker(r, movieFile.Offset(), movieFile.Length())
-			p := make([]byte, 1024)
-			n, err := rs.Read(p)
-			if err != nil {
-				log.WithError(err).Error("Error reading torrent file")
-				return
-			}
-			log.WithField("lenght", n).Info("Read number of bytes")
-		}()
+	// 		movieFile := getMovieFile(t)
+	// 		log.WithField("moviefile", movieFile.Path()).Info("Movie File")
 
-		for {
-			select {
-			case <-t.Closed():
-				fmt.Println("Torrent is closed.")
-			default:
-				fmt.Println("Torrent stats: ", t.Stats())
-				time.Sleep(time.Second)
-			}
-		}
+	// 		r := t.NewReader()
+	// 		defer r.Close()
 
-		return nil
-	}
+	// 		rs := missinggo.NewSectionReadSeeker(r, movieFile.Offset(), movieFile.Length())
+	// 		p := make([]byte, 1024)
+	// 		n, err := rs.Read(p)
+	// 		if err != nil {
+	// 			log.WithError(err).Error("Error reading torrent file")
+	// 			return
+	// 		}
+	// 		log.WithField("lenght", n).Info("Read number of bytes")
+	// 	}()
 
-	app.Run(os.Args)
-}
+	// 	for {
+	// 		select {
+	// 		case <-t.Closed():
+	// 			fmt.Println("Torrent is closed.")
+	// 		default:
+	// 			fmt.Println("Torrent stats: ", t.Stats())
+	// 			time.Sleep(time.Second)
+	// 		}
+	// 	}
 
-func getMovieFile(t *torrent.Torrent) *torrent.File {
-	var f torrent.File
-	var s int64
+	// 	return nil
+	// }
 
-	for _, file := range t.Files() {
-		if s < file.Length() {
-			s = file.Length()
-			f = file
-		}
-	}
-
-	return &f
+	// app.Run(os.Args)
 }
---
+++ torrent.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"io/ioutil"
+	"log"
+	"os"
+	"time"
+
+	"github.com/dustin/go-humanize"
+
+	"github.com/anacrolix/torrent"
+)
+
+var Client *torrent.Client
+
+func init() {
+	dataDir, err := ioutil.TempDir(os.TempDir(), "omckle")
+	if err != nil {
+		log.Fatalln("Cannot create temporary directory: ", err)
+	}
+	Client, err = torrent.NewClient(&torrent.Config{
+		DataDir: dataDir,
+		Debug:   true,
+	})
+	if err != nil {
+		log.Fatal("Error creating torrent client: ", err)
+	}
+	log.Println("Torrent data dir: ", dataDir)
+
+}
+
+func AddMagnetLink(magnet string) error {
+	t, err := Client.AddMagnet(magnet)
+	if err != nil {
+		return err
+	}
+
+	<-t.GotInfo()
+	t.DownloadAll()
+	log.Printf("Torrent added: %s\n", t.Name())
+
+	// Output torrent stats
+	go func() {
+		for {
+			select {
+			case <-t.Closed():
+				log.Println("Torrent ", t.Name(), " has been closed.")
+				return
+			case <-time.After(5 * time.Second):
+				info := t.Info()
+				log.Printf(`
+                    Torrent Stats:
+                        Name: %s
+                        Bytes Completed: %s
+                        Bytes Missing: %s
+                        Length: %s
+                `,
+					t.Name(),
+					humanize.Bytes(uint64(t.BytesCompleted())),
+					humanize.Bytes(uint64(t.BytesMissing())),
+					humanize.Bytes(uint64(info.TotalLength())))
+			}
+
+		}
+	}()
+	return nil
+}
+
+func getMovieFile(t *torrent.Torrent) *torrent.File {
+	var f torrent.File
+	var s int64
+
+	for _, file := range t.Files() {
+		if s < file.Length() {
+			s = file.Length()
+			f = file
+		}
+	}
+
+	return &f
+}
